33f046524a453cb06349e1409f8e3ed44fa106d2
Кирилл Мезенин <mezenin.97.ka@gmail.com>
2024-05-28 20:55:44 +1000

    Fix linter errors

--- src/Cli.php
+++ src/Cli.php
@@ -1,10 +1,13 @@
 <?php
+
 namespace BrainGames\Cli;
+
 use function cli\line;
 use function cli\prompt;
+
 function calluser()
 {
-line('Welcome to the Brain Game!');
-$name = prompt('May I have your name?');
-line("Hello, %s!", $name);
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
