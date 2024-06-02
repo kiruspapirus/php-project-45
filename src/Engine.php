6380ea683299c3af281f436c4154415e4ee1eb03
Кирилл Мезенин <mezenin.97.ka@gmail.com>
2024-06-02 14:23:44 +1000

    6th stage commit

---
+++ src/Engine.php
@@ -0,0 +1,29 @@
+<?php
+namespace BrainGames\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+function greeting()
+{
+    line('Welcome to the Brain Games!'); 
+    $name = prompt('May I have your name?'); 
+    line("Hello, %s!", $name); 
+    return $name;
+}
+
+function correct(): void
+{
+    line('Correct!');
+}
+
+function congratulations($name)
+{
+    line("Congratulations, $name!");
+}
+
+function error($userAnswer, $rightAnswer, $name)
+{
+    line("'$userAnswer' is wrong answer ;(. Correct answer was '$rightAnswer'.");
+    line("Let's try again, $name!");
+}
---
+++ src/Games/Calc.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace BrainGames\Games\Calc;
+
+use function cli\line;
+use function cli\prompt;
+use function BrainGames\Engine\greeting;
+use function BrainGames\Engine\correct;
+use function BrainGames\Engine\error;
+use function BrainGames\Engine\congratulations;
+
+function startGame()
+{
+    $name = greeting();
+    line('What is the result of the expression?');
+
+
+    $operation = ['+', '-', '*'];
+
+ for ($i = 0; $i < count($operation); $i++)
+    {
+      $number1 = rand(1, 99);
+      $number2 = rand(1, 99);
+      line('Question: ' . "$number1 " . $operation[$i] . " $number2");
+      if ($operation[$i] === '+')
+          {
+            $rightAnswer = $number1 + $number2;
+          }
+      elseif ($operation[$i] === '-')
+          {
+             $rightAnswer = $number1 - $number2;
+          }
+      else
+          {
+             $rightAnswer = $number1 * $number2;
+          }
+      $userAnswer = prompt('Your answer');
+      if ($userAnswer == $rightAnswer)
+        {
+             correct();
+        }
+      else {
+             error($userAnswer, $rightAnswer, $name);
+              return;
+           }
+    }
+        congratulations($name);
+}
+
